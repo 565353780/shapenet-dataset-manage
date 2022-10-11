2145e26176930a0936ee68fba945de9e9ae0f67b
chLi <565353780@qq.com>
2022-10-11 23:22:48 +0800

    update folder path

--- shapenet_dataset_manage/Demo/Core/V2/dataset_loader.py
+++ shapenet_dataset_manage/Demo/Core/V2/dataset_loader.py
@@ -5,13 +5,13 @@ from shapenet_dataset_manage.Module.Core.V2.dataset_loader import DatasetLoader
 
 
 def demo():
-    dataset_root_path = "/home/chli/scan2cad/shapenet/ShapeNetCore.v2/"
-    csv_file_path = "/home/chli/scan2cad/shapenet/all.csv"
+    dataset_root_path = "/home/chli/chLi/ShapeNet/ShapeNetCore.v2/"
+    csv_file_path = "/home/chli/chLi/ShapeNet/all.csv"
     msh2df_path = "/home/chli/github/local-deep-implicit-functions/ldif/gaps/bin/x86_64/msh2df"
     grd2msh_path = "/home/chli/github/local-deep-implicit-functions/ldif/gaps/bin/x86_64/grd2msh"
-    grd_save_folder_path = "/home/chli/scan2cad/shapenet/v2_grd/"
-    ply_save_folder_path = "/home/chli/scan2cad/shapenet/v2_ply/"
-    split_save_folder_path = "/home/chli/scan2cad/shapenet/v2_split/"
+    grd_save_folder_path = "/home/chli/ShapeNet/v2_grd/"
+    ply_save_folder_path = "/home/chli/ShapeNet/v2_ply/"
+    split_save_folder_path = "/home/chli/ShapeNet/v2_split/"
 
     dataset_loader = DatasetLoader()
     dataset_loader.loadDataset(dataset_root_path)
